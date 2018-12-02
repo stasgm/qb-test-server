6cd23d5dadd0b00c4992c4a6de5ae115a773bd76
Stanislau Shliakhtsich <stasgm@gmail.com>
2018-12-02 19:16:14 +0300

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 //const fs = require('fs');
 // const orm = require('gdmn-orm');
-import { deserializeERModel, EntityLink, EntityQuery, ERModel, Entity, Entities } from "gdmn-orm";
-import fetch from 'node-fetch';
+import {
+  deserializeERModel,
+  EntityLink,
+  EntityQuery,
+  ERModel,
+  Entity,
+  Entities,
+} from "gdmn-orm";
+import fetch from "node-fetch";
 
 /* const dbFile = "./src/db/db.json"; // db.json
 const data = fs.readFileSync(`./${dbFile}`, { encoding: "utf-8" });
@@ -10,19 +17,21 @@ const erModel = deserializeERModel(JSON.parse(data)); */
 const fetchData = async (): Promise<ERModel> => {
   const ermodelsURL = `http://localhost:8090`;
   const response = await fetch(ermodelsURL);
+
   if (!response.ok) throw new Error(response.statusText);
-  
-  const model = await response.json();
-  console.log(model.entities.GD_USER); 
+ 
+  const model = await deserializeERModel(await JSON.parse(await (response.json())));
+
   return model;
 };
 
 console.clear();
 
 let a: Entities;
+let ermodel: ERModel;
 
 fetchData()
-  .then(model => console.log(model.entity('GD_USER')))
+  .then(model => { console.log(model.entities); return ermodel})  
   .catch(e => console.log(`Ошибка: ${e.message}`));
 
 // const entity = erModel.entities.GD_USER;
@@ -35,4 +44,3 @@ fetchData()
 });
 console.log(el);
  */
-
